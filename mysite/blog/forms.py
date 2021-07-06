9db74e2b935b3133aa01538ae4b9fe52293aa306
chanck930 <86097504+chanck930@users.noreply.github.com>
2021-07-06 16:44:10 +0800

    Delete mysite/blog directory

--- mysite/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Entry
-
-class EntryForm(ModelForm):
-    class Meta: # Attach additional information
-        model = Entry
-        fields = ('text', )
