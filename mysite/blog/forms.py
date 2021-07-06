b493660201314365114133ef6df40ae69cec4ad7
chanck930 <86097504+chanck930@users.noreply.github.com>
2021-07-06 16:42:51 +0800

    Add files via upload

---
+++ mysite/blog/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Entry
+
+class EntryForm(ModelForm):
+    class Meta: # Attach additional information
+        model = Entry
+        fields = ('text', )
